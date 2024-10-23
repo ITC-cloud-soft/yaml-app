f04d3bb781c5c3f10c4c8ff548117ee452a8cd60
JackZouShao <jackzouyan@gmail.com>
2024-10-23 15:33:11 +0900

    recover wwwroot

--- Yaml/wwwroot/assets/js/common/uploadButton.js
+++
@@ -1,9 +0,0 @@
-function init (btn, spanElement, inputId){
-   $(btn).off("click").on("click", function(){
-       $(inputId).click()
-   }) 
-    
-    $(inputId).on("click", function(){
-        
-    })
-}
